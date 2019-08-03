1f8b6d68d4c9e42ff5ef4989909463a0648edbf9
nat-davydova <natti.davydova@gmail.com>
2019-08-03 13:37:02 +0300

    Nav: adding decoration block

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,26 @@
 const DOM = {
-  tabsNavList: document.querySelector('.tabs__nav-list') };
+  tabsNav: document.querySelector('.tabs__nav') };
+
+
+//appending decoration block to an active element
+const appendDecorationNav = () => {
+
+  //creating decoration element
+  let decorationElem = document.createElement('div');
+
+  decorationElem.classList.add('tabs__nav-decoration');
+  decorationElem.classList.add('js-decoration');
+
+  //appending element to navigation
+  DOM.tabsNav.append(decorationElem);
+
+
+};
+
+//onload function
+window.addEventListener('load', () => {
+
+  //appending decoration element to an active elem
+  appendDecorationNav();
+
+});
